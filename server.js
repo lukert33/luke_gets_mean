bb706daebaadc8ce977b871a46cd3bf892b22758
Luke Thomas <lukert33@gmail.com>
2015-02-18 14:44:11 -0500

    Set up basic crud routes in connected db.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//environment ======================================
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
@@ -5,8 +6,9 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var devPort = 8080;
+var db = mongoose.connection;
 
-mongoose.connect('mongodb://localhost/test');
+// app config ======================================
 
 app.use(express.static(__dirname + '/public'));
 
@@ -18,6 +20,65 @@ app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 
 app.use(methodOverride());
 
+//db with models======================================
+db.on('error', console.error);
+db.once('open', function(){
 
+  var Todo = mongoose.model('Todo', {
+    text: String
+  });
+
+});
+
+mongoose.connect('mongodb://localhost/test');
+//routes======================================
+  app.get('/todos', function(request, resolve){
+    Todo.find(function(err, todos){
+      if(err){
+        resolve.send(err);
+      }
+
+      resolve.json(todos);
+    });
+  });
+
+  app.post('/todos', function(request, resolve){
+    Todo.create({
+      text: request.body.text,
+      done: false
+    }, function(err, todo){
+      if(err){
+        resolve.send(err);
+      }
+
+      Todo.find(function(err, todos){
+        if(err){
+          resolve.send(err);
+        }
+        resolve.json(todos);
+      });
+    });
+  });
+
+  app.delete('todos/:todo_id', function(request, resolve){
+    Todo.remove({
+      _id : request.params.todo_id
+    }, function(err, todo){
+
+      if(err){
+        resolve.send(err);
+      }
+
+      Todo.find(function(err, todos){
+        if(err){
+          resolve.send(err);
+        }
+        resolve.json(todos);
+      });
+    });
+  });
+
+
+//listen======================================
 app.listen(devPort);
 console.log("App listening on port "+ devPort );
